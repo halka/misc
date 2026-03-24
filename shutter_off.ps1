a178aa9a4cc123355607b756a9766a5967e4a0aa
halka <halka@rjch.jp>
2026-03-24 11:27:00 +0900

    Update shutter_off.ps1

--- shutter_off.ps1
+++ shutter_off.ps1
@@ -1,7 +1,7 @@
-echo "SHUTTER SOUND KILLER"
-C:\Users\halka\AppData\Local\Microsoft\WinGet\Packages\Google.PlatformTools_Microsoft.Winget.Source_8wekyb3d8bbwe\platform-tools\adb.exe devices
+echo "SHUTTER SOUND KILLER for Samsung"
+adb devices
 echo "Wait 5 Secconds"
 sleep 5
 echo "COMMENCE!"
-C:\Users\halka\AppData\Local\Microsoft\WinGet\Packages\Google.PlatformTools_Microsoft.Winget.Source_8wekyb3d8bbwe\platform-tools\adb.exe shell settings put system csc_pref_camera_forced_shuttersound_key 0
+adb shell settings put system csc_pref_camera_forced_shuttersound_key 0
 echo "May Success! Check your Samsung's Phone!"
